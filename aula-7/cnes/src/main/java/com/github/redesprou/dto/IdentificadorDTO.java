b85ff6d87383010fee12cd49907e9a1ce7eca35d
Erivan <erivan.nas@gmail.com>
2018-11-05 21:42:52 -0200

    Adiciona DTO

---
+++ aula-7/cnes/src/main/java/com/github/redesprou/dto/IdentificadorDTO.java
@@ -0,0 +1,27 @@
+package com.github.redesprou.dto;
+
+import java.util.Date;
+
+public class IdentificadorDTO {
+	private Long id;
+	private Long individuoId;
+	private String designacao;
+	private Long area;
+	private String emissor;
+	private Date date;
+	private String tipo;
+	private Long certidaoId;
+	private Long tipoCertidao;
+	private String descricaoCertidao;
+	private String cartorioCertidao;
+	private Long livroCertidao;
+	private Long folhaCertidao;
+	private Long termoCertidao;
+	private Long tituloEleitoralId;
+	private Long zonaTitulo;
+	private Long sessaoTitulo;
+	private Long ctpsId;
+	private Long ctpsSerie;
+	private String ctpsEstado;
+	
+}
