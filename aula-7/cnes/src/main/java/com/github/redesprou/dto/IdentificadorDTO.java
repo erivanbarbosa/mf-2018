520092b8f8172bef042e1880dbc71d81fcf10c4b
Erivan <erivan.nas@gmail.com>
2018-11-09 08:15:37 -0200

    Getters and Setters

--- aula-7/cnes/src/main/java/com/github/redesprou/dto/IdentificadorDTO.java
+++ aula-7/cnes/src/main/java/com/github/redesprou/dto/IdentificadorDTO.java
@@ -23,5 +23,164 @@ public class IdentificadorDTO {
 	private Long ctpsId;
 	private Long ctpsSerie;
 	private String ctpsEstado;
-	
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getIndividuoId() {
+		return individuoId;
+	}
+
+	public void setIndividuoId(Long individuoId) {
+		this.individuoId = individuoId;
+	}
+
+	public String getDesignacao() {
+		return designacao;
+	}
+
+	public void setDesignacao(String designacao) {
+		this.designacao = designacao;
+	}
+
+	public Long getArea() {
+		return area;
+	}
+
+	public void setArea(Long area) {
+		this.area = area;
+	}
+
+	public String getEmissor() {
+		return emissor;
+	}
+
+	public void setEmissor(String emissor) {
+		this.emissor = emissor;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Long getCertidaoId() {
+		return certidaoId;
+	}
+
+	public void setCertidaoId(Long certidaoId) {
+		this.certidaoId = certidaoId;
+	}
+
+	public Long getTipoCertidao() {
+		return tipoCertidao;
+	}
+
+	public void setTipoCertidao(Long tipoCertidao) {
+		this.tipoCertidao = tipoCertidao;
+	}
+
+	public String getDescricaoCertidao() {
+		return descricaoCertidao;
+	}
+
+	public void setDescricaoCertidao(String descricaoCertidao) {
+		this.descricaoCertidao = descricaoCertidao;
+	}
+
+	public String getCartorioCertidao() {
+		return cartorioCertidao;
+	}
+
+	public void setCartorioCertidao(String cartorioCertidao) {
+		this.cartorioCertidao = cartorioCertidao;
+	}
+
+	public Long getLivroCertidao() {
+		return livroCertidao;
+	}
+
+	public void setLivroCertidao(Long livroCertidao) {
+		this.livroCertidao = livroCertidao;
+	}
+
+	public Long getFolhaCertidao() {
+		return folhaCertidao;
+	}
+
+	public void setFolhaCertidao(Long folhaCertidao) {
+		this.folhaCertidao = folhaCertidao;
+	}
+
+	public Long getTermoCertidao() {
+		return termoCertidao;
+	}
+
+	public void setTermoCertidao(Long termoCertidao) {
+		this.termoCertidao = termoCertidao;
+	}
+
+	public Long getTituloEleitoralId() {
+		return tituloEleitoralId;
+	}
+
+	public void setTituloEleitoralId(Long tituloEleitoralId) {
+		this.tituloEleitoralId = tituloEleitoralId;
+	}
+
+	public Long getZonaTitulo() {
+		return zonaTitulo;
+	}
+
+	public void setZonaTitulo(Long zonaTitulo) {
+		this.zonaTitulo = zonaTitulo;
+	}
+
+	public Long getSessaoTitulo() {
+		return sessaoTitulo;
+	}
+
+	public void setSessaoTitulo(Long sessaoTitulo) {
+		this.sessaoTitulo = sessaoTitulo;
+	}
+
+	public Long getCtpsId() {
+		return ctpsId;
+	}
+
+	public void setCtpsId(Long ctpsId) {
+		this.ctpsId = ctpsId;
+	}
+
+	public Long getCtpsSerie() {
+		return ctpsSerie;
+	}
+
+	public void setCtpsSerie(Long ctpsSerie) {
+		this.ctpsSerie = ctpsSerie;
+	}
+
+	public String getCtpsEstado() {
+		return ctpsEstado;
+	}
+
+	public void setCtpsEstado(String ctpsEstado) {
+		this.ctpsEstado = ctpsEstado;
+	}
 }
