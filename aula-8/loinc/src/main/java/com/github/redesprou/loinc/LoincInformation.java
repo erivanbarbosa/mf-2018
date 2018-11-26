4e51fbbe34744d0cdec6dae8bcb5eeb3d50f9385
Erivan <erivan.nas@gmail.com>
2018-11-26 13:51:31 -0200

    Implementa busca

---
+++ aula-8/loinc/src/main/java/com/github/redesprou/loinc/LoincInformation.java
@@ -0,0 +1,140 @@
+package com.github.redesprou.loinc;
+
+public class LoincInformation {
+	private Long num;
+	private String component;
+	private String property;
+	private String timeAspect;
+	private String systemInformation;
+	private String scaleType;
+	private String methodType;
+	private String classInformation;
+	private Integer classType;
+	private String longName;
+	private String shortName;
+	private String externalCopyrightNotice;
+	private String status;
+	private String versionFirstReleased;
+	private String versionLastChanged;
+
+	public Long getNum() {
+		return num;
+	}
+
+	public void setNum(Long num) {
+		this.num = num;
+	}
+
+	public String getComponent() {
+		return component;
+	}
+
+	public void setComponent(String component) {
+		this.component = component;
+	}
+
+	public String getProperty() {
+		return property;
+	}
+
+	public void setProperty(String property) {
+		this.property = property;
+	}
+
+	public String getTimeAspect() {
+		return timeAspect;
+	}
+
+	public void setTimeAspect(String timeAspect) {
+		this.timeAspect = timeAspect;
+	}
+
+	public String getSystemInformation() {
+		return systemInformation;
+	}
+
+	public void setSystemInformation(String systemInformation) {
+		this.systemInformation = systemInformation;
+	}
+
+	public String getScaleType() {
+		return scaleType;
+	}
+
+	public void setScaleType(String scaleType) {
+		this.scaleType = scaleType;
+	}
+
+	public String getMethodType() {
+		return methodType;
+	}
+
+	public void setMethodType(String methodType) {
+		this.methodType = methodType;
+	}
+
+	public String getClassInformation() {
+		return classInformation;
+	}
+
+	public void setClassInformation(String classInformation) {
+		this.classInformation = classInformation;
+	}
+
+	public Integer getClassType() {
+		return classType;
+	}
+
+	public void setClassType(Integer classType) {
+		this.classType = classType;
+	}
+
+	public String getLongName() {
+		return longName;
+	}
+
+	public void setLongName(String longName) {
+		this.longName = longName;
+	}
+
+	public String getShortName() {
+		return shortName;
+	}
+
+	public void setShortName(String shortName) {
+		this.shortName = shortName;
+	}
+
+	public String getExternalCopyrightNotice() {
+		return externalCopyrightNotice;
+	}
+
+	public void setExternalCopyrightNotice(String externalCopyrightNotice) {
+		this.externalCopyrightNotice = externalCopyrightNotice;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getVersionFirstReleased() {
+		return versionFirstReleased;
+	}
+
+	public void setVersionFirstReleased(String versionFirstReleased) {
+		this.versionFirstReleased = versionFirstReleased;
+	}
+
+	public String getVersionLastChanged() {
+		return versionLastChanged;
+	}
+
+	public void setVersionLastChanged(String versionLastChanged) {
+		this.versionLastChanged = versionLastChanged;
+	}
+
+}
---
+++ aula-8/loinc/src/main/java/com/github/redesprou/loinc/Searcher.java
@@ -0,0 +1,74 @@
+package com.github.redesprou.loinc;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import com.github.redesprou.util.FabricaConexao;
+
+public class Searcher {
+	public ArrayList<LoincInformation> pesquisar(String searchKeyWord) {
+
+		String pesquisaPorNome = "SELECT num, component, property, time_ascpect, system_information, scale_type, method_type, class_information, "
+				+ "	class_type, long_name, short_name, external_copyright_notice, status, version_first_released, version_last_changed "
+				+ " FROM loinc_core "
+				+ " WHERE UPPER(num) like ? OR UPPER(component) like ? OR UPPER(property) like ? OR UPPER(time_aspect like ? "
+				+ " OR UPPER(system_information) like ? OR UPPER(scale_type like) ? OR UPPER(method_type like) ? " 
+				+ " OR UPPER(class_information) like ? OR UPPER(long_name) like ?  OR UPPER(short_name like) ? "
+				+ " OR UPPER(external_copyright_notice like) ? OR UPPER(status like) ? OR UPPER(version_first_released like) ? "
+				+ " OR UPPER(version_last_changed) like ?";
+		LoincInformation loincInformation = null;
+
+		ArrayList<LoincInformation> resultList = new ArrayList<LoincInformation>();
+
+		try {
+			Connection conexao = FabricaConexao.getConexao();
+			PreparedStatement consulta = conexao.prepareStatement(pesquisaPorNome);
+
+			consulta.setString(1, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(2, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(3, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(4, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(5, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(6, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(7, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(8, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(9, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(10, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(11, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(12, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(13, "%" + searchKeyWord.toUpperCase() + "%");
+			consulta.setString(14, "%" + searchKeyWord.toUpperCase() + "%");
+			ResultSet resultado = consulta.executeQuery();
+
+			while (resultado.next()) {
+				loincInformation = new LoincInformation();
+				loincInformation.setNum(resultado.getLong("NUM"));
+
+				loincInformation.setComponent(resultado.getString("COMPONENT"));
+				loincInformation.setProperty(resultado.getString("PROPERTY"));
+				loincInformation.setTimeAspect(resultado.getString("TIME_ASCPECT"));
+				loincInformation.setSystemInformation(resultado.getString("SYSTEM_INFORMATION"));
+				loincInformation.setScaleType(resultado.getString("SCALE_TYPE"));
+				loincInformation.setMethodType(resultado.getString("METHOD_TYPE"));
+				loincInformation.setClassInformation(resultado.getString("CLASS_INFORMATION"));
+				loincInformation.setClassType(resultado.getInt("CLASS_TYPE"));
+				loincInformation.setLongName(resultado.getString("LONG_NAME"));
+				loincInformation.setShortName(resultado.getString("SHORT_NAME"));
+				loincInformation.setExternalCopyrightNotice(resultado.getString("EXTERNAL_COPYRIGHT_NOTICE"));
+				loincInformation.setStatus(resultado.getString("STATUS"));
+				loincInformation.setVersionFirstReleased(resultado.getString("VERSION_FIRST_RELEASED"));
+				loincInformation.setVersionLastChanged(resultado.getString("VERSION_LAST_CHANGED"));
+
+				resultList.add(loincInformation);
+			}
+			resultado.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return resultList;
+
+	}
+}
